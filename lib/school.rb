20a166fc626e9b131a9fc721f680c24fb60f1866
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-27 22:03:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+  
+  # Used attr_reader to cuz initialize is going to make the setter
+  attr_reader :school, :roster;
+  
+  # Init w/ name and empty roster(hash)
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  # Define method for adding a student
+  def add_student(name, grade)
+    roster[grade]||=[]
+    roster[grade] << name
+  end
+  
+  # Gives the Roster for a grade level
+  def grade(class)
+    roster.detect do |x,y|
+      if x == class
+         y
+      end
+    end
+  end
+  
+  # Sorts the roster by name
+  
+  
+  
+  
+end
